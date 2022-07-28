fa9bc9fc038a0f2702c07f5fdf877e9b5ee33a20
Ben Jones <ben.jones12@gmail.com>
2022-07-29 10:35:09 +1200

    Revert "Add spi bus support (#2)"
    This reverts commit d3f89629afb2ca255802eae06487f0f2c8170631.

--- src/Ethernet.h
+++ src/Ethernet.h
@@ -49,7 +49,6 @@
 
 
 #include <Arduino.h>
-#include <SPI.h>
 #include "Client.h"
 #include "Server.h"
 #include "Udp.h"
@@ -109,14 +108,6 @@ public:
 	friend class EthernetClient;
 	friend class EthernetServer;
 	friend class EthernetUDP;
-
-	// Get SPI class handle
-#ifdef ARDUINO_ARCH_SAMD
-	static   SPIClassSAMD& getSPIinstance(void); // Get SPI class handle
-#else
-	static   SPIClass& getSPIinstance(void); // Get SPI class handle
-#endif
-
 private:
 	// Opens a socket(TCP or UDP or IP_RAW mode)
 	static uint8_t socketBegin(uint8_t protocol, uint16_t port);
